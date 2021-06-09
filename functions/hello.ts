047072582008f3881cc2b894b1daa3e1726eb793
ivorpad <ivorjpc@gmail.com>
2021-06-09 13:29:54 +0200

    Added envs

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,14 +1,19 @@
 import { Handler } from "@netlify/functions";
 
-console.log(process.env);
-
 const handler: Handler = async function (event, context) {
   const { identity, user } = context.clientContext;
+  console.log(identity, user);
    return {
      statusCode: 200,
      body: JSON.stringify({
        message: "Hello World",
-       variables: process.env,
+       email: process.env.CONTACT_FORM_EMAIL,
+       toml_env_variable: process.env.TEST_VALUE || "not found",
+       event: {
+         multiValueQueryStringParameters: event.multiValueQueryStringParameters,
+         queryStringParameters: event.queryStringParameters,
+         body: event.body,
+       },
      }),
    };
 };
